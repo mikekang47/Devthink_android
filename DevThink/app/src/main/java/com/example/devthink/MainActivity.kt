fd2e988663c063d6773147a73e412fa1dd52bd61
hong0708 <gaogai2@naver.com>
2022-01-17 13:57:06 +0900

    Add: bottom_nav_menu

--- DevThink/app/src/main/java/com/example/devthink/MainActivity.kt
+++ DevThink/app/src/main/java/com/example/devthink/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.devthink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.devthink.community.CommunityFragment
+import com.example.devthink.databinding.ActivityMainBinding
+import com.example.devthink.home.HomeFragment
+import com.example.devthink.setting.SettingFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bnvMain.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.communityBtn -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, CommunityFragment()).commit()
+                }
+                R.id.settingBtn -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, SettingFragment()).commit()
+                }
+                R.id.homeBtn -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, HomeFragment()).commit()
+                }
+            }
+            true
+        }
+
+        // 초기 화면
+        binding.bnvMain.selectedItemId = R.id.homeBtn
     }
 }
---
+++ DevThink/app/src/main/java/com/example/devthink/setting/SettingFragment.kt
@@ -0,0 +1,20 @@
+package com.example.devthink.setting
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.devthink.databinding.FragmentSettingBinding
+
+class SettingFragment : Fragment() {
+    lateinit var binding: FragmentSettingBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
